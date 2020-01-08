b1a2ba8d0d043b89eb3ec8bd8f041ac9dd69b13c
happou31 <happo31@gmail.com>
2020-01-08 21:10:35 +0900

    fix: ちゃんと動くようになった

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ LABEL maintainer "happou31 <happo31@gmail.com>"
 USER root
 
 ADD conf/nginx/acme-challenge.conf /etc/nginx/conf.d/
+ADD conf/nginx/ssl-site.conf.in /etc/nginx/conf.d/
 
 RUN touch /tmp/once \
   && mkdir /usr/share/nginx/html/letsencrypt \
--- scripts/run_cert.sh
+++ scripts/run_cert.sh
@@ -11,6 +11,9 @@ if [ -e /tmp/once ]; then
       -m "$EMAIL" \
       -d "$DOMAIN" \
 
+  cat /etc/nginx/conf.d/ssl-site.conf.in | sed -e "s/@@domain_name@@/$DOMAIN/" > /etc/nginx/conf.d/ssl-site.conf
+  rm /etc/nginx/conf.d/ssl-site.conf.in
+
   echo "0 0,12 * * * root python -c 'import random; import time; time.sleep(random.random() * 3600)' && certbot renew" | tee -a /etc/crontab > /dev/null
 
   rm /tmp/once
