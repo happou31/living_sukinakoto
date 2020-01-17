0bf214bf7ab2098b6089885780be3d5f0cd3c4fd
happou31 <happo31@gmail.com>
2020-01-18 02:04:38 +0900

    add: 環境変数からポート番号を読み込むように

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 
+const port = process.env.NODE_PORT || "3000";
 const app = express();
 
-const server = app.listen(3000, () => {
+const server = app.listen(Number.parseInt(port), () => {
   const addr = server.address();
   if (addr) {
     if (typeof addr === "string") {
